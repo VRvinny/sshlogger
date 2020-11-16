af66284ec5e610b7cf1b2cb0cfc7b5d603b174ee
vinny <you@example.com>
2020-11-16 20:28:04 +0000

    added time stamps on ip addresses

--- logger.sh
+++ logger.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd /home/scripts/sshlogger
+
 touch usernames.txt
 touch ipaddrs.txt
 
 #get the usernames and ip addresses from /var/log/auth.log
-users=$(cat /var/log/auth.log | grep "sshd" | grep "$(date +"%b %d")" | grep "Invalid" | sed -n "s/^.*user\s*\(\S*\).*$/\1/p")
+users=$(cat auth.log | grep "sshd" | grep "$(date +"%b %d")" | grep "Invalid" | sed -n "s/^.*user\s*\(\S*\).*$/\1/p")
 
-cat /var/log/auth.log | grep "sshd" | grep "$(date +"%b %d")" | grep "Invalid" | sed -n "s/^.*from\s*\(\S*\).*$/\1/p" >> ipaddrs.txt
+cat auth.log | grep "sshd" | grep "$(date +"%b %d")" | grep "Invalid" | awk '{print $10 " " $1 " " $2 " " $3 }' >> ipaddrs.txt
 
 # only add unique usernames to file
 for line in $(echo "$users" | sed -e 's/ /\n/g'); do grep -q $line usernames.txt || echo $line >> usernames.txt; done 
